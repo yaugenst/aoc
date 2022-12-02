8dab8341b2b33934afeabf9e20453f01a44bd3e4
mrbaozi <mrbaozi@me.com>
2022-12-02 06:48:38 +0100

    Day 2 python version - what a disgrace

---
+++ 2022/day02/day02.py
@@ -0,0 +1,21 @@
+data = open("input.txt").read()
+
+points = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+beat = {"C": "X", "A": "Y", "B": "Z"}
+draw = {"A": "X", "B": "Y", "C": "Z"}
+lose = {"B": "X", "C": "Y", "A": "Z"}
+
+score = 0
+for line in data.strip().split("\n"):
+    a, b = line[0], line[2]
+
+    # # uncomment for part 2
+    # b = {"X": lose, "Y": draw, "Z": beat}[b][a]
+
+    score += points[b]
+    if b == draw[a]:
+        score += 3
+    if b == beat[a]:
+        score += 6
+
+print(score)

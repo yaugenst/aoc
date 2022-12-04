8ded59c4a6d90254e381939381f8329290029657
mrbaozi <mrbaozi@me.com>
2022-12-04 06:43:03 +0100

    Add day 4 julia solution

---
+++ 2022/day04/day04.jl
@@ -0,0 +1,13 @@
+function main()
+    data = filter(x -> !isempty(x), split(readchomp("input.txt"), "\n"))
+
+    mapreduce(+, data) do x
+        m = parse.(Int, match(r"(\d+)-(\d+),(\d+)-(\d+)", x))
+        a, b = m[1]:m[2], m[3]:m[4]
+
+        (a ⊆ b) | (b ⊆ a)  # part 1
+        # !isdisjoint(a, b)  # part 2
+    end
+end
+
+main()

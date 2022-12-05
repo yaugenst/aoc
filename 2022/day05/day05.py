e00511a388fa04a654662478bdd663094940ab84
mrbaozi <mrbaozi@me.com>
2022-12-05 09:19:17 +0100

    Remove unnecessary brackets

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -2,14 +2,14 @@ import re
 
 stacks, moves = open("input.txt").read().split("\n\n")
 
-sd = {k: [] for k in map(int, re.findall(r"[(\d+)]", stacks.split("\n")[-1]))}
+sd = {k: [] for k in map(int, re.findall(r"[\d+]", stacks.split("\n")[-1]))}
 for line in stacks.split("\n")[:-1]:
     for n, idx in enumerate(range(1, len(line), 4)):
         if line[idx] != " ":
             sd[n + 1].append(line[idx])
 
 for move in moves.split("\n"):
-    a, b, c = map(int, re.findall(r"(\d+)", move))
+    a, b, c = map(int, re.findall(r"\d+", move))
 
     # part 1
     sd[c] = [sd[b].pop(0) for _ in range(a)][::-1] + sd[c]

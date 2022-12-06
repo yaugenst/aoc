3edd4fea4cb60f7b734ead3c47573919adfc2442
mrbaozi <mrbaozi@me.com>
2022-12-06 06:45:11 +0100

    Fix little indexing mistake

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -1,7 +1,7 @@
 data = open("input.txt").read().strip()
 
 n = 4  # 14 for part 2
-for idx in range(len(data[:-4])):
+for idx in range(len(data[:-n])):
     if len(set(data[idx : idx + n])) == n:
         print(idx + n)
         break

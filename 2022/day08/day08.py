bdc0c6d12e5316e25e35acaa6f5c9f6217d4a68c
mrbaozi <mrbaozi@me.com>
2022-12-08 07:45:27 +0100

    Refactored the whole thing

--- 2022/day08/day08.py
+++ 2022/day08/day08.py
@@ -4,42 +4,28 @@ grid = []
 for line in data:
     grid.append([int(x) for x in list(line)])
 
-# part 1
 visible = 2 * (len(grid) + len(grid[0]) - 2)
-for i in range(1, len(grid) - 1):
-    for j in range(1, len(grid) - 1):
-        gij = grid[i][j]
-        visible += (
-            all(gij > g for g in grid[i][:j])
-            | all(gij > g for g in grid[i][j + 1 :])
-            | all(gij > g for g in (grid[k][j] for k in range(i)))
-            | all(gij > g for g in (grid[k][j] for k in range(i + 1, len(grid[0]))))
-        )
-print(visible)
-
-# part 2
 scores = []
 for i in range(1, len(grid) - 1):
     for j in range(1, len(grid) - 1):
-        l = 0
-        for g in grid[i][:j][::-1]:
-            l += 1
-            if g >= grid[i][j]:
-                break
-        r = 0
-        for g in grid[i][j + 1 :]:
-            r += 1
-            if g >= grid[i][j]:
-                break
-        t = 0
-        for g in [grid[k][j] for k in range(i)][::-1]:
-            t += 1
-            if g >= grid[i][j]:
-                break
-        b = 0
-        for g in [grid[k][j] for k in range(i + 1, len(grid[0]))]:
-            b += 1
-            if g >= grid[i][j]:
-                break
-        scores.append(l * r * t * b)
+        v = False
+        scores.append(1)
+        for search_direction in [
+            grid[i][:j][::-1],
+            grid[i][j + 1 :],
+            [grid[k][j] for k in range(i)][::-1],
+            [grid[k][j] for k in range(i + 1, len(grid[0]))],
+        ]:
+            # part 1
+            v |= all(grid[i][j] > g for g in search_direction)
+
+            # part 2
+            score = 0
+            for tree in search_direction:
+                score += 1
+                if tree >= grid[i][j]:
+                    break
+            scores[-1] *= score
+        visible += v
+print(visible)
 print(max(scores))

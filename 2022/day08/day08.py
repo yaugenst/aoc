189aad0f5044754e4dbdcf390e58c14f1281bd7e
mrbaozi <mrbaozi@me.com>
2022-12-08 06:57:59 +0100

    At least make part 1 and part 2 visible

--- 2022/day08/day08.py
+++ 2022/day08/day08.py
@@ -3,10 +3,22 @@ data = open("input.txt").read().strip().split("\n")
 grid = []
 for line in data:
     grid.append([int(x) for x in list(line)])
-print(grid)
 
+# part 1
+visible = 2 * (len(grid) + len(grid[0]) - 2)
+for i in range(1, len(grid) - 1):
+    for j in range(1, len(grid) - 1):
+        h = all(grid[i][j] > g for g in grid[i][:j]) or all(
+            grid[i][j] > g for g in grid[i][j + 1 :]
+        )
+        v = all(grid[i][j] > g for g in [grid[k][j] for k in range(i)]) or all(
+            grid[i][j] > g for g in [grid[k][j] for k in range(i + 1, len(grid[0]))]
+        )
+        visible += h or v
+print(visible)
+
+# part 2
 scores = []
-visible = 2 * len(grid) + 2 * len(grid[0]) - 4
 for i in range(1, len(grid) - 1):
     for j in range(1, len(grid) - 1):
         l = 0
@@ -15,7 +27,6 @@ for i in range(1, len(grid) - 1):
             if g >= grid[i][j]:
                 break
         r = 0
-
         for ii, g in enumerate(grid[i][j + 1 :]):
             r += 1
             if g >= grid[i][j]:
@@ -31,22 +42,4 @@ for i in range(1, len(grid) - 1):
             if g >= grid[i][j]:
                 break
         scores.append(l * r * t * b)
-        # exit()
-        # h = all(grid[i][j] > g for g in grid[i][:j]) or all(
-        #     grid[i][j] > g for g in grid[i][j + 1 :]
-        # )
-        # v = all(grid[i][j] > g for g in [grid[k][j] for k in range(i)]) or all(
-        #     grid[i][j] > g for g in [grid[k][j] for k in range(i + 1, len(grid[0]))]
-        # )
-        # print(i, j)
-        # print(list(range(j)))
-        # print([g for g in [grid[k][j] for k in range(i)]])
-        # print([g for g in [grid[k][j] for k in range(i + 1, len(grid[0]))]])
-        # continue
-        # exit()
-        # # print([grid[i][j] > g for g in [grid[k][j] for k in range(j)]])
-        # print(i, j, h or v)
-        # visible += h or v
-
-# print(visible)
 print(max(scores))

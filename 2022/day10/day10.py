e8e84026a502e15fe1f1c48ab4f528d3697e5781
mrbaozi <mrbaozi@me.com>
2022-12-10 08:22:23 +0100

    Can do it way neater by just parsing smarter

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -1,23 +1,15 @@
-data = open("input.txt").read().strip().split("\n")
-cycles = {"addx": 2, "noop": 1}
+import textwrap
+
+data = open("input.txt").read().strip()
+data = data.replace("addx", "0").replace("noop", "0").replace("\n", " ")
+instructions = map(int, data.split())
 
 x = 1
-cycle = 1
-result = 0
-nx = 40
-for line in data:
-    for _ in range(cycles[line[:4]]):
-        # part 1
-        if cycle == 20 or (cycle + 20) % 40 == 0:
-            result += x * cycle
-
-        # part 2
-        c = "#" if cycle % nx in range(x, x + 3) else "."
-        print(c, end="")
-        if cycle % nx == 0:
-            print()
-
-        cycle += 1
-    if line[:4] == "addx":
-        x += int(line[5:])
-print(result)
+part1 = 0
+part2 = ""
+for i, v in enumerate(instructions, 1):
+    part1 += i * x if i % 40 == 20 else 0
+    part2 += "#" if i % 40 in range(x, x + 3) else "."
+    x += v
+print(textwrap.fill(part2, 40))
+print(part1)

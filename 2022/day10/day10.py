5aa75a2c581c6b2976e770b4261b8066386a91c9
mrbaozi <mrbaozi@me.com>
2022-12-10 08:25:54 +0100

    And now you can actually read the letters

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -9,7 +9,7 @@ part1 = 0
 part2 = ""
 for i, v in enumerate(instructions, 1):
     part1 += i * x if i % 40 == 20 else 0
-    part2 += "#" if i % 40 in range(x, x + 3) else "."
+    part2 += "⬜" if i % 40 in range(x, x + 3) else "⬛"
     x += v
 print(textwrap.fill(part2, 40))
 print(part1)
